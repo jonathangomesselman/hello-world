bc593456ff64275f2df3c97a5e201a890a7857c3
jonathangomesselman <jgs8@stanford.edu>
2017-07-03 15:29:14 -0400

    Added main

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 first repository
 Well this is just a test so blah blah blah
 Change again
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Jonathan Gomes Selman on 7/3/17.
+//
+
+using namespace std;
+
+int main() {
+   cout << "hell" << endl;
+    return 0;
+}
